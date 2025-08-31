9ae7ac32141dc0cdf38f5bc0a2f805e14d438be1
Howphile <Howphile@gmail.com>
2025-08-31 13:24:28 -0400

    Added score

--- Assets/Scripts/PlayerData.cs
+++ Assets/Scripts/PlayerData.cs
@@ -1,22 +1,31 @@
+using TMPro;
 using UnityEngine;
 
 public class PlayerData : MonoBehaviour
 {
 
+    [SerializeField] private GameObject textObj;
+    private TMP_Text textMeshPro;
+
     [SerializeField] private float coins = 0f;
     void Start()
     {
-        
+        textMeshPro = textObj.GetComponent<TMP_Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateCoins();
     }
 
     public void AddCoins(int amount)
     {
         coins += amount;
+    }
+
+    private void UpdateCoins()
+    {
+        textMeshPro.text = coins.ToString();
     }
 }
